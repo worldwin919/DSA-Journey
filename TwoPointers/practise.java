813d975ea183a90f9287ea5d1f3832b7a77f0029
worldwin919 <nayalvishvajeet@gmail.com>
2025-12-02 22:39:06 +0530

    added while infite loop

--- TwoPointers/practise.java
+++ TwoPointers/practise.java
@@ -1,6 +1,8 @@
 public class practise{
   public static void main(String[]args){
      System.out.println("Hi This is just a practise session");
-     System.out.println("after a long time doing this")
+     System.out.println("after a long time doing this");
+     int x = 3, y = 5;
+     while(true);
   }
 }
